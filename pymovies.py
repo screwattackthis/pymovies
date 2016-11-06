7844518fee90d1e9962ac252818f75e30cee6d1b
screwattackthis <screwattackthis@users.noreply.github.com>
2016-11-06 01:16:51 -0700

    Added python-magic to check if file is a video

--- pymovies.py
+++ pymovies.py
@@ -1,15 +1,18 @@
 from guessit import guessit
 import os
+import magic
 
 root = '.'
 for directory, subdirectories, files in os.walk(root):
     for file in files:
         try:
             full_path = os.path.join(directory, file)
-            file_info = os.stat(full_path)
-            if file_info.st_nlink == 1:
-                print('\tFound: {}'.format(ascii(full_path)))
-                movie_match = guessit(full_path)
-                print("\tMatched with {}".format(movie_match['title']))
+            mime = magic.Magic(full_path)
+            if 'video' in mime:
+                file_info = os.stat(full_path)
+                if file_info.st_nlink == 1:
+                    print('\tFound: {}'.format(ascii(full_path)))
+                    movie_match = guessit(full_path)
+                    print("\tMatched with {}".format(movie_match['title']))
         except FileNotFoundError:
             print("File not found, skipping...")
