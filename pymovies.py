5000af2381520eedfa16afb4f1d521c19619da68
screwattackthis <screwattackthis@users.noreply.github.com>
2016-11-06 01:22:54 -0700

    Correctly using python-magic module

--- pymovies.py
+++ pymovies.py
@@ -3,12 +3,13 @@ import os
 import magic
 
 root = '.'
+mime = magic.Magic(mime=True)
 for directory, subdirectories, files in os.walk(root):
     for file in files:
         try:
             full_path = os.path.join(directory, file)
-            mime = magic.Magic(full_path)
-            if 'video' in mime:
+            file_mime = mime.from_file(full_path)
+            if 'video' in file_mime:
                 file_info = os.stat(full_path)
                 if file_info.st_nlink == 1:
                     print('\tFound: {}'.format(ascii(full_path)))
