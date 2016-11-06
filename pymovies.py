0a249af87092bb22e77f3bb030c83b42c0f261a3
screwattackthis <screwattackthis@users.noreply.github.com>
2016-11-06 01:49:21 -0600

    Formatting and fixed pathing for files in subdirectories

--- pymovies.py
+++ pymovies.py
@@ -7,6 +7,7 @@ for directory, subdirectories, files in os.walk(root):
     for file in files:
         file_info = os.stat(file)
         if file_info.st_nlink == 1:
-            print('Found file {} and matching...'.format(ascii(file), 'ignore'))
-            movie_match = guessit(file)
-            print("Matched with {}".format(movie_match['title']))
+            full_path = os.path.join(directory, file)
+            print('\tFound: {}'.format(ascii(full_path)))
+            movie_match = guessit(full_path)
+            print("\tMatched with {}".format(movie_match['title']))
