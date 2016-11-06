5bf7ab7cf04fd8a69e51ca0e794f79660b35371c
screwattackthis <screwattackthis@users.noreply.github.com>
2016-11-06 02:54:24 -0700

    Removed python-magic, added minimum size, checks if file extension is in approved list

--- pymovies.py
+++ pymovies.py
@@ -1,19 +1,18 @@
 from guessit import guessit
 import os
-import magic
 
 root = '.'
-mime = magic.Magic(mime=True)
+min_size = 200000000
+video_exts = ['.mkv', '.mp4']
 for directory, subdirectories, files in os.walk(root):
     for file in files:
         try:
             full_path = os.path.join(directory, file)
-            file_mime = mime.from_file(full_path)
-            if 'video' in file_mime:
-                file_info = os.stat(full_path)
-                if file_info.st_nlink == 1:
-                    print('\tFound: {}'.format(ascii(full_path)))
-                    movie_match = guessit(full_path)
-                    print("\tMatched with {}".format(movie_match['title']))
+            file_name, file_ext = os.path.splitext(file)
+            file_info = os.stat(full_path)
+            if file_ext in video_exts and file_info.st_nlink == 1 and file_info.st_size > min_size:
+                print('\tFound: {}'.format(ascii(full_path)))
+                movie_match = guessit(file)
+                print("\tMatched with {}".format(movie_match['title']))
         except FileNotFoundError:
             print("File not found, skipping...")
