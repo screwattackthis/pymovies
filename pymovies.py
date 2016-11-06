3931d28cd816e2ebfa580fc511ba042b3077af7f
screwattackthis <screwattackthis@users.noreply.github.com>
2016-11-06 01:24:48 -0600

    Initial commit

---
+++ pymovies.py
@@ -0,0 +1,12 @@
+from guessit import guessit
+import os
+
+root = '.'
+for directory, subdirectories, files in os.walk(root):
+    print('Directory: {}'.format(directory))
+    for file in files:
+        file_info = os.stat(file)
+        if file_info.st_nlink > 1:
+            print('Found file {} and matching...'.format(file))
+            movie_match = guessit(file)
+            print("Matched with {}".format(movie_match['title']))
