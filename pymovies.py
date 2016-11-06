2aea76fc9e023f1a457ba132ebe080eae06e10f2
screwattackthis <screwattackthis@users.noreply.github.com>
2016-11-06 01:59:27 -0600

    Error handling for file not found

--- pymovies.py
+++ pymovies.py
@@ -5,12 +5,12 @@ root = '.'
 for directory, subdirectories, files in os.walk(root):
     print('Directory: {}'.format(ascii(directory)))
     for file in files:
-        file_info = os.stat(file)
-        if file_info.st_nlink == 1:
-            full_path = os.path.join(directory, file)
-            print('\tFound: {}'.format(ascii(full_path)))
-            try:
+        try:
+            file_info = os.stat(file)
+            if file_info.st_nlink == 1:
+                full_path = os.path.join(directory, file)
+                print('\tFound: {}'.format(ascii(full_path)))
                 movie_match = guessit(full_path)
                 print("\tMatched with {}".format(movie_match['title']))
-            except FileNotFoundError:
-                print("File not found, skipping...")
+        except FileNotFoundError:
+            print("File not found, skipping...")
