b1264bf7194978800f2fc2d798fe7d49620eaede
screwattackthis <screwattackthis@users.noreply.github.com>
2016-11-06 01:47:01 -0600

    Added ascii conversion for printing file name

--- pymovies.py
+++ pymovies.py
@@ -7,6 +7,6 @@ for directory, subdirectories, files in os.walk(root):
     for file in files:
         file_info = os.stat(file)
         if file_info.st_nlink == 1:
-            print('Found file {} and matching...'.format(file, 'ignore'))
+            print('Found file {} and matching...'.format(ascii(file), 'ignore'))
             movie_match = guessit(file)
             print("Matched with {}".format(movie_match['title']))
