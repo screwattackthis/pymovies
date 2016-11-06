1f1b32e1d62346bd093bc2c6eb6b45a7f9f5855d
screwattackthis <screwattackthis@users.noreply.github.com>
2016-11-06 04:00:28 -0700

    Hardlinks correctly

--- pymovies.py
+++ pymovies.py
@@ -1,10 +1,12 @@
 from guessit import guessit
 import os
 
-root = '.'
+source = './source'
+destination = './destination'
 min_size = 200000000
 video_exts = ['.mkv', '.mp4']
-for directory, subdirectories, files in os.walk(root):
+results = {}
+for directory, subdirectories, files in os.walk(source):
     for file in files:
         try:
             full_path = os.path.join(directory, file)
@@ -13,6 +15,16 @@ for directory, subdirectories, files in os.walk(root):
             if file_ext in video_exts and file_info.st_nlink == 1 and file_info.st_size > min_size:
                 print('\tFound: {}'.format(ascii(full_path)))
                 movie_match = guessit(file)
-                print("\tMatched with {}".format(movie_match['title']))
+                new_directory = "{} ({}) [{}]".format(movie_match['title'], movie_match['year'], movie_match['screen_size'])
+                print("\tMatched with {}".format(new_directory))
+                results[full_path] = (destination, new_directory, file)
+                confirm = input("\n\tCreate hard link? (Y\\n)\n\t\t{}\n".format(os.path.join(destination, new_directory)))
+                if confirm is 'Y':
+                    os.mkdir(os.path.join(destination, new_directory))
+                    os.link(full_path, os.path.join(destination, new_directory, file))
+                    print('\t\tHard link created succesfully.')
+                else:
+                    print('\t\tSkipping {}'.format(new_directory))
+
         except FileNotFoundError:
             print("File not found, skipping...")
